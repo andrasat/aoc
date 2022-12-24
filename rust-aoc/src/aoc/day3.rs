2e46d460345105904d075a9822031ff33faf0bf3
andrasat <andra.satria1@gmail.com>
2022-12-24 19:54:43 +0700

    completed day 3 solution

--- rust-aoc/src/aoc/day3.rs
+++ rust-aoc/src/aoc/day3.rs
@@ -52,23 +52,50 @@ pub fn day_3() {
     let mut second_rucksack_group = "";
     let mut third_rucksack_group = "";
 
-    let count: u16 = 0;
+    let mut count: u16 = 0;
     for row in split_input_two {
         count += 1;
 
-        // let first_comp_chars: Vec<char> = first_compartment.chars().collect();
+        if count % 3 == 0 {
+            first_rucksack_group = row;
+        } else if count % 2 == 0 {
+            second_rucksack_group = row;
+        } else {
+            third_rucksack_group = row;
+        }
 
-        // for first_comp_char in first_comp_chars {
-        //     if second_compartment.contains(first_comp_char) {
-        //         let ascii_num = first_comp_char as u8;
-        //         if ascii_num >= 97 {
-        //             rearrangement_vec.push(ascii_num - 96);
-        //             break;
-        //         } else if ascii_num >= 65 {
-        //             rearrangement_vec.push(ascii_num - 38);
-        //             break;
-        //         }
-        //     }
-        // }
+        if first_rucksack_group != "" && second_rucksack_group != "" && third_rucksack_group != "" {
+            let first_rucksack_chars: Vec<char> = first_rucksack_group.chars().collect();
+
+            for first_char in first_rucksack_chars {
+                if second_rucksack_group.contains(first_char)
+                    && third_rucksack_group.contains(first_char)
+                {
+                    let ascii_num = first_char as u8;
+                    if ascii_num >= 97 {
+                        all_group_vec.push(ascii_num - 96);
+                        break;
+                    } else if ascii_num >= 65 {
+                        all_group_vec.push(ascii_num - 38);
+                        break;
+                    }
+                }
+            }
+
+            first_rucksack_group = "";
+            second_rucksack_group = "";
+            third_rucksack_group = "";
+        }
     }
+
+    let sum: u64 = match all_group_vec
+        .iter()
+        .map(|v| v.to_owned() as u64)
+        .reduce(|sum: u64, v: u64| sum + v)
+    {
+        Some(v) => v,
+        None => 0,
+    };
+
+    println!("sum group: {}", sum)
 }
