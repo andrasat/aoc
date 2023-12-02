019ebec32db95eb43d9fdcf1dfe56793c27bb555
andrasat <andra.satria1@gmail.com>
2023-12-02 17:20:35 +0700

    finish 2023 day 2 part 1

--- ocaml_aoc/lib/2023/day2.ml
+++ ocaml_aoc/lib/2023/day2.ml
@@ -29,57 +29,49 @@ let get_game_id input =
   Int.of_string raw_game_id
 ;;
 
-let match_and_sum_gems acc gem =
-  match gem with
-  | x when String.is_suffix ~suffix:"red" x ->
-    { acc with
-      red = acc.red + (String.sub x ~pos:0 ~len:2 |> String.strip |> Int.of_string)
-    }
-  | x when String.is_suffix ~suffix:"green" x ->
-    { acc with
-      green = acc.green + (String.sub x ~pos:0 ~len:2 |> String.strip |> Int.of_string)
-    }
-  | x when String.is_suffix ~suffix:"blue" x ->
-    { acc with
-      blue = acc.blue + (String.sub x ~pos:0 ~len:2 |> String.strip |> Int.of_string)
-    }
+let extract_gem_count gem = String.sub gem ~pos:0 ~len:2 |> String.strip |> Int.of_string
+
+let generate_bag_of_gem acc gem =
+  match String.strip gem with
+  | x when String.is_suffix ~suffix:"red" x -> { acc with red = extract_gem_count x }
+  | x when String.is_suffix ~suffix:"green" x -> { acc with green = extract_gem_count x }
+  | x when String.is_suffix ~suffix:"blue" x -> { acc with blue = extract_gem_count x }
   | _ -> acc
 ;;
 
 let get_one_bag input =
   String.strip input
   |> String.split ~on:','
-  |> List.fold_left ~init:{ red = 0; green = 0; blue = 0 } ~f:match_and_sum_gems
+  |> List.fold_left ~init:{ red = 0; green = 0; blue = 0 } ~f:generate_bag_of_gem
 ;;
 
-let check_if_bag_of_gems_possible input =
+let get_highest_possible_bag_of_gem input =
   let empty_bag = { red = 0; green = 0; blue = 0 } in
   let game_records = String.split ~on:';' input in
-  let rec get_sum_of_bags records sum_of_bags =
+  let rec find_highest_gem_in_bag records max_bag_of_gem =
     match records with
-    | [] -> sum_of_bags
+    | [] -> max_bag_of_gem
     | r :: rs ->
       let bag = get_one_bag r in
-      get_sum_of_bags
+      find_highest_gem_in_bag
         rs
-        { red = sum_of_bags.red + bag.red
-        ; green = sum_of_bags.green + bag.green
-        ; blue = sum_of_bags.blue + bag.blue
+        { red = (if bag.red > max_bag_of_gem.red then bag.red else max_bag_of_gem.red)
+        ; green =
+            (if bag.green > max_bag_of_gem.green then bag.green else max_bag_of_gem.green)
+        ; blue =
+            (if bag.blue > max_bag_of_gem.blue then bag.blue else max_bag_of_gem.blue)
         }
   in
-  let final_bag = get_sum_of_bags game_records empty_bag in
-  printf "final_bag: %d, %d, %d\n" final_bag.red final_bag.green final_bag.blue;
-  if final_bag.red < 12 && final_bag.green < 13 && final_bag.blue < 14
-  then true
-  else false
+  find_highest_gem_in_bag game_records empty_bag
 ;;
 
 let get_game_id_when_sum_of_gems_are_valid input =
   let input_list = String.split ~on:':' input in
   let game_id = get_game_id (List.hd_exn input_list) in
-  let is_possible = input_list |> List.last_exn |> check_if_bag_of_gems_possible in
-  printf "game_id: %d, is_possible: %b\n" game_id is_possible;
-  if is_possible then game_id else 0
+  let bag_of_gem = input_list |> List.last_exn |> get_highest_possible_bag_of_gem in
+  if bag_of_gem.red <= 12 && bag_of_gem.green <= 13 && bag_of_gem.blue <= 14
+  then game_id
+  else 0
 ;;
 
 let rec part_one_solution input_list =
