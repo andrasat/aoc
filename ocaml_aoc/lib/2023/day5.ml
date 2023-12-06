9236a910cfd1818f5714775a0e4465a9d4790a52
andra <andra.satria1@gmail.com>
2023-12-06 11:28:22 +0700

    WIP: day 5 part 2

--- ocaml_aoc/lib/2023/day5.ml
+++ ocaml_aoc/lib/2023/day5.ml
@@ -1,13 +1,32 @@
 open Core
 
-let parse_data input =
+let parse_seeds_normal seeds =
+  seeds
+  |> Str.replace_first (Str.regexp "seeds: ") ""
+  |> String.split ~on:' '
+  |> List.map ~f:Int.of_string
+;;
+
+let rec generate_seeds_range (seeds_list : string list) =
+  match seeds_list with
+  | [] -> []
+  | [ _ ] -> failwith "Invalid seeds"
+  | start :: length :: rest -> [ start; length ] :: generate_seeds_range rest
+;;
+
+let parse_seeds_range seeds =
+  seeds
+  |> Str.replace_first (Str.regexp "seeds: ") ""
+  |> String.split ~on:' '
+  |> generate_seeds_range
+  |> List.map ~f:(List.map ~f:Int.of_string)
+;;
+
+let parse_data input ~seed_parser =
   match input |> Str.split (Str.regexp "\n\n") with
   | [] -> failwith "No data"
   | seeds :: maps ->
-    ( seeds
-      |> Str.replace_first (Str.regexp "seeds: ") ""
-      |> String.split ~on:' '
-      |> List.map ~f:Int.of_string
+    ( seed_parser seeds
     , maps
       |> List.map ~f:(String.split ~on:'\n')
       |> List.map ~f:List.tl_exn
@@ -34,7 +53,7 @@ let rec find_location loc (maps : int list list list) =
         | _ -> failwith "Invalid map"))
 ;;
 
-let part_one ((seeds : int list), (maps : int list list list)) =
+let find_lowest_location ((seeds : int list), (maps : int list list list)) =
   List.fold
     ~init:0
     ~f:(fun lowest seed ->
@@ -46,12 +65,44 @@ let part_one ((seeds : int list), (maps : int list list list)) =
     seeds
 ;;
 
+let part_one input =
+  let seeds, maps = parse_data input ~seed_parser:parse_seeds_normal in
+  find_lowest_location (seeds, maps)
+;;
+
+let rec find_lowest_location_v2 loc ((seeds : int list list), (maps : int list list list))
+  =
+  match seeds with
+  | [] -> loc
+  | seed_range :: rest ->
+    let seed_start = List.hd_exn seed_range in
+    let seed_length = List.nth_exn seed_range 1 in
+    let range = List.init (seed_length - 1) ~f:(fun i -> seed_start + i) in
+    printf
+      "Range, %d-%d, start: %d length: %d\n"
+      (List.hd_exn range)
+      (List.last_exn range)
+      seed_start
+      seed_length;
+    let new_loc = find_lowest_location (range, maps) in
+    if loc = 0 || new_loc < loc
+    then find_lowest_location_v2 new_loc (rest, maps)
+    else find_lowest_location_v2 loc (rest, maps)
+;;
+
+let part_two input =
+  let seeds, maps = parse_data input ~seed_parser:parse_seeds_range in
+  find_lowest_location_v2 0 (seeds, maps)
+;;
+
+(* find_lowest_location (seeds, maps) *)
+
 module Exec = struct
   let run () =
     print_endline "Day 5:";
     let input = Helper.FileReader.read_file "../inputs/2023/day5-puzzle.txt" in
-    let seeds, maps = parse_data input in
-    printf "Part 1: %d\n" (part_one (seeds, maps));
+    printf "Part 1: %d\n" (part_one input);
+    printf "Part 2: %d\n" (part_two input);
     print_endline "-------------------------"
   ;;
 end
